1914c8adf15bbd3467ba99bd66e2bfc46b97a28e
Moreno <moreno.zolfo@gmail.com>
2021-09-17 10:33:24 +0900

    updated cross-compare to track prevalences /1

--- CRISPR_spacers_lookup/cross-compare_SGB_and_VSC_abd.py
+++ CRISPR_spacers_lookup/cross-compare_SGB_and_VSC_abd.py
@@ -25,7 +25,11 @@ parser.add_argument('--vsc_mp_samples',help="The list of 18k metagenomes used in
 
 parser.add_argument('--vscs_prevalence',help="The VSCs prevalence file",default="data/VSGs_prevalence_2021.csv")
 parser.add_argument('--vscs_crisprs',help="The VSCs CRISPRs file",default="data/VSGs_crispr_2021.csv")
+parser.add_argument('--nk',help="Filter this number of SGB/CRISPR associations. If a Viral group has less than this number of CRISPR hits, it will be excluded", default=5,type=int)
 
+
+#Most Prevalent
+##M251 M1206 M1102 M1057 M1159 M367 M311 M1085 M1174 M1221 M1219 M1169 M1180 M1222 M1188 M349 M1240 M428 M75 M59 M1202 M704 M326 M1216 M1149 M892 M697 M282 M441 M302 M229 M1064 M1110 M808 M867 M769 M801 M727 M460 M738 M412 M459 M696 M923 M1063 M646 M914 M756 M302 M900 
 args = parser.parse_args()
 
 def split_index(a):
@@ -52,6 +56,8 @@ VSC_CRISPRS = pd.read_table(args.vscs_crisprs,header=0,index_col=0)
 
 
 VSC_prevalences_dict={}
+prevalenceDataFrameDict=[]
+VSC_SGB_targets = {}
 
 for target in args.targetV:
 	print("Target:", target)
@@ -64,7 +70,7 @@ for target in args.targetV:
 	MGroup_type = MGroup_description[0] #this is k or u
 
 	#all the SGB with alignments with this M-Group (SGBID, SGB-String):
-	VSC_SGB_targets = dict( \
+	VSC_SGB_targets[target] = dict( \
 		(SBGString.split(' ')[0][1:], \
 		( \
 			SBGString, \
@@ -85,18 +91,26 @@ for target in args.targetV:
 	#this is: all the metagenomes analyzed that were stool samples (n=18714)
 	samples_to_compare_with_SGB = set(stool_samples_in_vsc_analysis).intersection(set(samples_screened_in_vsc_analysis))
 
+	print('CNT stool_samples_in_vsc_analysis: ',len(stool_samples_in_vsc_analysis))
+	print('CNT samples_screened_in_vsc_analysis: ',len(samples_screened_in_vsc_analysis))
+	print('CNT samples_to_compare_with_SGB: ',len(samples_to_compare_with_SGB))
 
 
-	tgt = avct.loc[target]
+	TOTAL_SMPLS = len(samples_to_compare_with_SGB)
+	SGB_prevalences_dict = {'with_virus':{},'no_virus':{}}
+
+	
+	tgt = avct.loc[target] 
+
 	tgt_with_m = list(tgt[tgt > 0.75].index)
 	 
 
 
 	abdDF=[]
 
-	for target_sgb,(target_sgb_stats, target_sgb_hits, target_sgb_totalCount) in VSC_SGB_targets.items():
+	for target_sgb,(target_sgb_stats, target_sgb_hits, target_sgb_totalCount) in VSC_SGB_targets[target].items():
 
-		if target_sgb_hits < 5: continue
+		if target_sgb_hits < args.nk: continue
 		print("Wk sgb ",target_sgb,(target_sgb_stats, target_sgb_hits, target_sgb_totalCount))
 
 
@@ -111,15 +125,34 @@ for target in args.targetV:
 				if len(abd):
 					sgb_abundance = abd[0]
 					if (float(sgb_abundance) > 0):
+
+						if sample in tgt_with_m:
+							SGB_prevalences_dict['with_virus'][sample] = 1
+						elif sample not in tgt_with_m:
+							SGB_prevalences_dict['no_virus'][sample] = 1
+
+
 						abdDF.append({'SGB':target_sgb,'sample': sample,'sgb_abundance': sgb_abundance,'phage_present': 'Yes' if sample in tgt_with_m else 'No'})
 						overall_abds.append({'target':target,'type':MGroup_type,'SGB':target_sgb,'sample': sample,'sgb_abundance': sgb_abundance,'phage_present': 'Yes' if sample in tgt_with_m else 'No'})
 	
 
-	abdDataFrame = pd.pivot_table(pd.DataFrame.from_dict(abdDF),index=['SGB','sample','phage_present'],values='sgb_abundance',aggfunc=sum).reset_index()
-	print(abdDataFrame)
+	prevalenceDataFrameDict.append({'target':target, \
+		'prevalence': len(SGB_prevalences_dict['with_virus']) / TOTAL_SMPLS *100, \
+		'phage_present': 'Yes' \
+		})
+	prevalenceDataFrameDict.append({'target':target, \
+		'prevalence': len(SGB_prevalences_dict['no_virus']) / TOTAL_SMPLS *100, \
+		'phage_present': 'No' \
+		})
 
-	if not abdDataFrame.empty:
 
+	abdDataFramePrev = pd.DataFrame.from_dict(abdDF)
+
+
+	if not abdDataFramePrev.empty:
+
+		abdDataFrame = pd.pivot_table(abdDataFramePrev,index=['SGB','sample','phage_present'],values='sgb_abundance',aggfunc=sum).reset_index()
+		print(abdDataFrame)
 
 		astats=[]
 		for target_sgb in set(abdDataFrame['SGB']): 
@@ -180,14 +213,16 @@ for target in args.targetV:
 
 
 overall_abdsPD = pd.DataFrame.from_dict(overall_abds)
-f, axx = plt.subplots(2,1,figsize=(20,10))
 
+prevalenceDataFrame  = pd.DataFrame.from_dict(prevalenceDataFrameDict)
+print("EXX", prevalenceDataFrame)
 
-f1=0
-
+f, axx = plt.subplots(4,1,figsize=(25,10), gridspec_kw={'height_ratios': [1.5,3,1.5,3]})
+ 
 orverallStats=[]
 
 passedGroups=[]
+f1=0
 for typer,tgt in overall_abdsPD.groupby('type'):
 
 	print(typer,tgt)
@@ -220,10 +255,15 @@ for typer,tgt in overall_abdsPD.groupby('type'):
 
 		print("STA", len(yess),len(no))
 
-		orverallStats.append({'M-Group':stat_tgt,'Samples_With':len(yess),'Samples_Without':len(no),'Median_With': np.median(yess),'Median_Without': np.median(no),'p-value (t)': stats.ttest_ind(yess,no).pvalue,'p-value (welch)': stats.ttest_ind(yess,no,equal_var=False).pvalue, 'p-value (mwu)': stats.mannwhitneyu(yess,no).pvalue})
+		if len(yess) > 0 and len(no) > 0:
+			orverallStats.append({'M-Group':stat_tgt,'Samples_With':len(yess),'Samples_Without':len(no),'Median_With': np.median(yess),'Median_Without': np.median(no),'p-value (t)': stats.ttest_ind(yess,no).pvalue,'p-value (welch)': stats.ttest_ind(yess,no,equal_var=False).pvalue, 'p-value (mwu)': stats.mannwhitneyu(yess,no).pvalue})
+		else:
+			orverallStats.append({'M-Group':stat_tgt,'Samples_With':len(yess),'Samples_Without':len(no),'Median_With': np.median(yess)})
+
 		print(orverallStats)
 	
-	ax=axx[f1]
+	ax_hist=axx[f1]
+	ax=axx[f1+1]
 
 
 	targets = set(tgt_piv['target'])
@@ -232,28 +272,47 @@ for typer,tgt in overall_abdsPD.groupby('type'):
 	print("Orderded:",odr)
 
 
-	sns.boxplot(x='target',y='sgb_abundance',hue='phage_present',hue_order=['No','Yes'],order=odr,data=tgt_piv,palette="Reds" if typer == 'u' else 'Blues', linewidth=1,fliersize=1.1,ax=ax)
+	sns.barplot(x='target',y='prevalence',hue='phage_present',hue_order=['No','Yes'], order=odr,data=prevalenceDataFrame,palette="Reds" if typer == 'u' else 'Blues', edgecolor=".2",  linewidth=2, ax=ax_hist)
+	sns.boxplot(x='target',y='sgb_abundance',hue='phage_present',hue_order=['No','Yes'],order=odr,data=tgt_piv,palette="Reds" if typer == 'u' else 'Blues',ax=ax)
 	
 	#ax.set_xticklabels(['Yes','No'])
 	ax.set_yscale("log")
-	ax.axhline(3.39245)
-	ax.axhline(11.47173,c='r')
 
 	ax.set(ylim=(0.01,100))
 	ax.set_ylabel('Sum of rel. abundances [%]', fontsize=10)
-	ax.set_xlabel('VSC Group', fontsize=10)
+	ax.set_xlabel('Viral Group', fontsize=10)
 	#ax.minorticks_on()
 	ax.tick_params(axis='y', which='minor', bottom=False)
-	sns.despine()
+
+
+	ax_hist.set_yscale("log")
+	ax_hist.set(ylim=(0.1,100))
+	ax_hist.set_ylabel('Combined SGB prevalence [%]', fontsize=10)
+	ax_hist.set_xlabel('Viral Group', fontsize=10)
+	ax_hist.tick_params(axis='y', which='minor', bottom=False)
+
+	#Put VSC prevalences on Xlabels
+	nl=[]
+	for item in ax_hist.get_xticklabels():
+		nl.append( '{}\n{:.2f}%\n{}'.format( \
+			item.get_text(), \
+			VSC_prevalences_dict[item.get_text()] *100, \
+			len(VSC_SGB_targets[item.get_text()]))) 
+	ax_hist.set_xticklabels(nl)
+
+ 
 	#ax.set_title('Most prevalent kVSGs' if typer == 'k' else '' , fontsize=10)
 
-	f1+=1
-	
+	f1+=2
+
+sns.despine()
+
 for ax in f.axes:
 	#plt.setp(ax.get_yticklabels(), visible=True)
 	ax.yaxis.set_major_formatter(mtick.PercentFormatter(100,decimals=2))
 	#ax.xaxis.set_tick_params(which='both', labelbottom=True)
 	#ax.yaxis.set_tick_params(which='both', labelleft=True)
+
 
 
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
